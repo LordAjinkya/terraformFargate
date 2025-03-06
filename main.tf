036ea4a29012be4bea5326b53ac3c460d7305792
Ajinkya <ajinkyanarale007@gmail.com>
2025-03-06 23:16:44 +0530

    removed the lines of main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,1 @@
-module "network" {
-  source = "./network"
-}
-
-module "alb" {
-  source = "./alb"
-}
-
-module "ecs" {
-  source = "./ecs"
-}
-
-module "ecs_task_definition" {
-  source = "./ecs_task_definition"
-}
+#
